87c8a0e2939b58086b8e87c0adc56d6857ad4b8b
Santiago Negreira <sansfleitas@gmail.com>
2024-07-30 11:57:11 -0300

    Create product

---
+++ application/product.go
@@ -0,0 +1,57 @@
+package application
+
+import "errors"
+
+type ProductInterface interface {
+	IsValid() (bool, error)
+	Enable() error
+	Disable() error
+	GetID() string
+	GetName() string
+	GetStatus() string
+	GetPrice() float64
+}
+
+const (
+	DISABLED = "disabled"
+	ENABLED  = "enabled"
+)
+
+type Product struct {
+	ID     string
+	Name   string
+	Price  float64
+	Status string
+}
+
+//func (p *Product) IsValid() (bool, error) {
+//
+//}
+
+func (p *Product) Enable() error {
+	if p.Price > 0 {
+		p.Status = ENABLED
+		return nil
+	}
+	return errors.New("the price must be greater than zero to enable the product")
+}
+
+//func (p *Product) Disable() error {
+//
+//}
+
+func (p *Product) GetID() string {
+	return p.ID
+}
+
+func (p *Product) GetName() string {
+	return p.Name
+}
+
+func (p *Product) GetStatus() string {
+	return p.Status
+}
+
+func (p *Product) GetPrice() float64 {
+	return p.Price
+}
---
+++ application/product_test.go
@@ -0,0 +1,21 @@
+package application_test
+
+import (
+	"github.com/Sans-arch/fc-arquitetura-hexagonal/application"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
